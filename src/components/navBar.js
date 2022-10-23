e8023a57046e04ef4c8c231531246022eab051b7
David Andreessen <davidandreessen@yahoo.com>
2022-10-23 19:40:51 -0400

    added Nav Bar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,12 +1,38 @@
-import React from 'react';
 import './NavBar.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NavBar = () => {
-    return (
-        <div className="navBar" >
+
+const Navbar = ({ logout, token }) => {
+  return (
+    <header>
+      <nav class="navbar">
+        <Link class="link" to='/'>Home</Link>
+        <Link class="link" to='/Routines'>Routines</Link>
+
+{
+token ?(
+  <>
+<Link class="link" to='/' 
+          onClick={ () => {
+            logout()
+        }}>Logout</Link>
         
- </div>
-    )
+<Link class="link" to='/routines/EditRoutine'>Edit Routine</Link>
+<Link class="link" to='/MyRoutines'>My Routines</Link>
+</>
+      
+): (<>
+  <Link class="link" to='/register'>Register</Link>
+  <Link class="link" to='/login'>Login</Link>
+
+  </>
+)
+}
+      </nav>
+     
+    </header>
+  )
 }
 
-export default NavBar;
+export default Navbar;
